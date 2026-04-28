76e0e57bbf98ccb54a48fadc211b1fb226a5275a
mpsteenstrup <mpsteenstrup@gmail.com>
2026-04-28 22:11:17 +0200

    spil

---
+++ escaperoom/opgaver.js
@@ -0,0 +1,22 @@
+const levels = [
+    {
+        title: "Level 1: Newtons Love",
+        description: "Find det skjulte tal i billedet og divider det med massen (2 kg).",
+        answer: "5",
+        image: "morris.png" ,
+        music: "musik1.mp3"
+    },
+    {
+        title: "Level 2: Bølgelængde",
+        description: "Beregn frekvensen for det grønne lys fra bilaget.",
+        answer: "gul",
+        image: "morris.png", 
+        pdf: "eks1.pdf"
+    },
+    {
+        title: "Level 3: Atomfysik",
+        description: "Hvad kaldes overgangen fra n=3 til n=2 i brintatomet?",
+        answer: "h-alpha",
+        image: "spektrum.png"
+    }
+];
