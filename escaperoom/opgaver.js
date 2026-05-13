8e6a66d912936fdc2e8afbf625e47f2cb2f7d794
mpsteenstrup <mpsteenstrup@gmail.com>
2026-05-13 23:03:09 +0200

    atomer

--- escaperoom/opgaver.js
+++ escaperoom/opgaver.js
@@ -1,23 +1,32 @@
 const levels = [
     {
-        title: "Atomers fingeraftryk",
-        description: "Se de flotte farver. Hvad kan mon lave sådan noget? for at finde ud skal I bestemme hvad det er og i rækkefølge. Måske kan denne hjemmeside hjælpe jer: http://www.hyperphysics.phy-astr.gsu.edu/hbase/quantum/atspect3.html#c1",
-        answer: "NeHeHg",
-        image: "grundstoffer.png" ,
-        music: "musik1.mp3",
-        pdf: "grundstoffer.pdf"
-    },
-    {
-        title: "Lys og energi",
-        description: "Ja ja I er videre! Men hvordan er det nu at vi kan se det lys? hvad er lys og hvorfor har det farver? Det er vist noget med bølger, men hvem udsender den korteste ja og næstkorteste og næst næst korteste?",
-        answer: "HgHeNe",
-        image: "grundstoffer.png",
+        title: "Level 1: Lys og energi",
+        description: "",
+        answer: "321",
+        image: "grundstoffer3.png",
         music: "musik1.mp3"
     },
     {
-        title: "Level 3: Atomfysik",
-        description: "Hvad kaldes overgangen fra n=3 til n=2 i brintatomet?",
-        answer: "h-alpha",
-        image: "spektrum.png"
+        title: "Level 2: Atomfysik",
+        description: "",
+        answer: "emission",
+        image: "grundstoffer4.png",
+        music: "musik1.mp3"
+    },
+    {
+        title: "Level 3: Atomers fingeraftryk",
+        description: "Det lyser og skinner, men hvad kommer det af?",
+        answer: "NeHeHg",
+        image: "grundstoffer2.png" ,
+        music: "musik1.mp3",
+        pdf: "grundstoffer.pdf"
+    },
+    
+        {
+        title: "Level 4: Atomfysik",
+        description: "",
+        answer: "",
+        image: "grundstoffer5.png"
     }
+
 ];
