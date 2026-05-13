cd99c3a8dc05b31773f61b762cbfde125b59fd97
mpsteenstrup <mpsteenstrup@gmail.com>
2026-05-13 12:58:38 +0200

    escaperoom

--- escaperoom/opgaver.js
+++ escaperoom/opgaver.js
@@ -4,14 +4,14 @@ const levels = [
         description: "Se de flotte farver. Hvad kan mon lave sådan noget? for at finde ud skal I bestemme hvad det er og i rækkefølge. Måske kan denne hjemmeside hjælpe jer: http://www.hyperphysics.phy-astr.gsu.edu/hbase/quantum/atspect3.html#c1",
         answer: "NeHeHg",
         image: "grundstoffer.png" ,
-        music: "musik1.mp3"
+        music: "musik1.mp3",
         pdf: "grundstoffer.pdf"
     },
     {
         title: "Lys og energi",
         description: "Ja ja I er videre! Men hvordan er det nu at vi kan se det lys? hvad er lys og hvorfor har det farver? Det er vist noget med bølger, men hvem udsender den korteste ja og næstkorteste og næst næst korteste?",
         answer: "HgHeNe",
-        image: "grundstoffer.png" ,
+        image: "grundstoffer.png",
         music: "musik1.mp3"
     },
     {
